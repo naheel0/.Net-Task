a6a9c12f251be1855756dfbfbcec3e96cb5c6f25
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-02-05 10:42:24 +0530

    add task 1

---
+++ Employee/Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeApi.Models;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> Employees = new List<Employee>
+        {
+             new Employee { Id = 1, Name = "John", Department = "IT",  },
+             new Employee { Id = 2, Name = "Sara", Department = "HR",  }
+        };
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            return Ok(Employees);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+            return Ok(employee);
+        }
+        [HttpPut("{Id}")]
+        public IActionResult EditEmployee(int Id, Employee UpdatedEmployee)
+        {
+            var employee = Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = UpdatedEmployee.Name;
+            employee.Department = UpdatedEmployee.Department;
+            return Ok(employee);
+        }
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteEmployee(int Id)
+        {
+            var employee = Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            Employees.Remove(employee);
+            return Ok(Employees);
+        }
+    }
+}
---
+++ Employee/Employee/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+     public int Id { set; get; }
+        public string Name { set; get; } = string.Empty;
+        public string Department { set; get; } = string.Empty;
+    }
+}
